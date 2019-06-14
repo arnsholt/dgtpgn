16f3cb5615344126f78c5631f9c8f1ca48b44b63
Arne Skjærholt <arnsholt@gmail.com>
2019-06-14 10:06:55 +0200

    First pass at full recording functionality.

--- src/main/java/org/riisholt/dgtpgn/DgtPgn.java
+++ src/main/java/org/riisholt/dgtpgn/DgtPgn.java
@@ -1,47 +1,102 @@
 package org.riisholt.dgtpgn;
 
-import org.openmuc.jrxtx.*;
+import com.fazecast.jSerialComm.SerialPort;
 
 import org.riisholt.dgtdriver.DgtDriver;
 import org.riisholt.dgtdriver.moveparser.Game;
 import org.riisholt.dgtdriver.moveparser.MoveParser;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import java.text.SimpleDateFormat;
+
+import java.util.Arrays;
+import java.util.Date;
+
 public class DgtPgn {
-    public static void main(String[] argv) throws java.io.IOException {
-        // TODO: Validate args.
-
-        new DgtPgn(argv[0]).run();
-    }
-
-    //private SerialPort port;
-    //private DgtDriver driver;
-    private DgtPgn(String portName) throws java.io.IOException {
-        System.out.println(portName);
-        SerialPort port = SerialPortBuilder.newBuilder(portName)
-                                .setDataBits(DataBits.DATABITS_8)
-                                .setBaudRate(9600)
-                                .setStopBits(StopBits.STOPBITS_1)
-                                .setParity(Parity.NONE)
-                                .setFlowControl(FlowControl.NONE)
-                                .build();
-        MoveParser parser = new MoveParser(this::gameComplete);
-        DgtDriver driver = new DgtDriver(parser::gotMove, (bytes) -> writeBytes(port, bytes));
-    }
-
-    private void run() {
-        System.out.println("Hello world?");
-    }
-
-    public void gameComplete(Game g) {
-        // TODO: Write PGN to file.
-    }
-
-    public void writeBytes(SerialPort port, byte[] bytes) {
-        try {
-            port.getOutputStream().write(bytes);
+    public static void main(String[] argv) throws IOException, InterruptedException {
+        String prefix = new SimpleDateFormat("yyyy-MM-dd-HHmmss").format(new Date());
+        String portName;
+        if(argv.length == 1) {
+            portName = argv[0];
         }
-        catch(java.io.IOException e) {
-            System.err.println("Failed to write!"); // XXX: Improve
+        else if(argv.length == 2) {
+            portName = argv[0];
+            prefix = argv[1];
+        }
+        else {
+            System.err.println("Usage: dgtpgn PORT [PREFIX]");
+            return;
+        }
+
+        new DgtPgn(portName, prefix).run();
+    }
+
+    private SerialPort port;
+    private DgtDriver driver;
+    private String outputPrefix;
+    private int gameCount = 0;
+    private DgtPgn(String portName, String prefix) {
+        outputPrefix = prefix;
+        System.out.printf("Connecting to %s\n", portName);
+        port = SerialPort.getCommPort(portName);
+        port.setBaudRate(9600);
+        port.setParity(SerialPort.NO_PARITY);
+        port.setNumDataBits(8);
+        port.setNumStopBits(1);
+        if(!port.openPort())
+            throw new RuntimeException("Failed to open port.");
+        MoveParser parser = new MoveParser(this::gameComplete);
+        driver = new DgtDriver(parser::gotMessage, (bytes) -> writeBytes(port, bytes));
+    }
+
+    private void run() throws IOException, InterruptedException {
+        driver.board();
+        driver.clock();
+        driver.updateNice();
+        InputStream in = port.getInputStream();
+        byte[] buffer = new byte[128];
+        while (true) {
+            int read = in.read(buffer, 0, in.available());
+            if(read == -1)
+                break;
+            else if(read == 0) {
+                Thread.sleep(250);
+            }
+            else {
+                driver.gotBytes(Arrays.copyOf(buffer, read));
+            }
+        }
+    }
+
+    private void gameComplete(Game g) {
+        try {
+            gameCount++;
+            FileWriter writer = new FileWriter(String.format("%s-%d.pgn", outputPrefix, gameCount));
+            writer.write(g.pgn(true));
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void writeBytes(SerialPort port, byte[] bytes) {
+        try {
+            System.out.println(port.isOpen());
+            OutputStream s = port.getOutputStream();
+            if(s != null) {
+                s.write(bytes);
+                s.close();
+            }
+            else {
+                System.err.println("Failed to get output stream from port");
+            }
+        }
+        catch(IOException e) {
+            System.err.printf("Failed to write: %s", e.getMessage());
         }
     }
 }
