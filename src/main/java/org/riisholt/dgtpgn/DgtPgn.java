d3fed4b1796edc0e95eae3a07abde19b4dcd6b34
Arne Skjaerholt <arnsholt@gmail.com>
2022-12-12 09:59:40 +0100

    Prettier display.

--- src/main/java/org/riisholt/dgtpgn/DgtPgn.java
+++ src/main/java/org/riisholt/dgtpgn/DgtPgn.java
@@ -2,12 +2,18 @@ package org.riisholt.dgtpgn;
 
 import net.sourceforge.argparse4j.inf.*;
 
+import org.riisholt.dgtdriver.BWTime;
 import org.riisholt.dgtdriver.DgtDriver;
 import org.riisholt.dgtdriver.DgtMessage;
+import org.riisholt.dgtdriver.game.Piece;
+import org.riisholt.dgtdriver.game.Role;
+import org.riisholt.dgtdriver.game.Square;
 import org.riisholt.dgtdriver.moveparser.Game;
 import org.riisholt.dgtdriver.moveparser.MoveParser;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class DgtPgn extends Program {
     public static void main(String[] argv) throws IOException, InterruptedException {
@@ -48,6 +54,21 @@ public class DgtPgn extends Program {
 
     protected void gotMessage(DgtMessage msg) {
         parser.gotMessage(msg);
+        Game game = parser.currentGame(null);
+
+        if(!(game != null && game.moves.size() > 0))
+            return;
+
+        try {
+            printBoard(parser.boardState());
+            printMoves(game);
+            BWTime clockInfo = game.moves.get(game.moves.size() - 1).clockInfo;
+            if(clockInfo != null)
+                printClocks(clockInfo);
+        }
+        catch(IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     protected void shutdownHook() {
@@ -69,5 +90,72 @@ public class DgtPgn extends Program {
         catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void printBoard(org.riisholt.dgtdriver.game.Board board) throws IOException {
+        clearScreen();
+
+        // Board frame
+        at(1, 1, "+--------+");
+        at(2, 1, "|");
+        at(3, 1, "|");
+        at(4, 1, "|");
+        at(5, 1, "|");
+        at(6, 1, "|");
+        at(7, 1, "|");
+        at(8, 1, "|");
+        at(9, 1, "|");
+        at(2, 10, "|");
+        at(3, 10, "|");
+        at(4, 10, "|");
+        at(5, 10, "|");
+        at(6, 10, "|");
+        at(7, 10, "|");
+        at(8, 10, "|");
+        at(9, 10, "|");
+        at(10, 1, "+--------+");
+
+        // Pieces
+        for(Map.Entry<Integer, Piece> entry: board.pieceMap().entrySet()) {
+            Integer square = entry.getKey();
+            int rank = Square.rank(square);
+            int file = Square.file(square);
+            Piece piece = entry.getValue();
+            String symbol = piece.role.symbol;
+            if(piece.role == Role.PAWN)
+                symbol = "P";
+            at(9 - rank, file + 2, piece.white? symbol: symbol.toLowerCase());
+        }
+    }
+
+    private void printMoves(Game game) throws IOException {
+        ArrayList<String> moves = new ArrayList<>();
+        for(int i = 0; 2*i < game.moves.size(); i++) {
+            String white = game.moves.get(2*i).san;
+            String black = 2*i + 1 < game.moves.size()? game.moves.get(2*i + 1).san : "";
+            moves.add(String.format("%3d. %-7s %-7s", i + 1, white, black));
+        }
+
+        int curRow = 1;
+        int curCol = 12;
+        for(String s: moves) {
+            at(curRow, curCol, s);
+            curRow += 1;
+        }
+    }
+
+    private void printClocks(BWTime clocks) throws IOException {
+        String whiteTime = clocks.leftTimeString();
+        String blackTime = clocks.rightTimeString();
+        at(11, 1, whiteTime);
+        at(12, 4, blackTime);
+    }
+
+    private void at(int x, int y, String s) {
+        System.out.printf("\u001B[%d;%dH%s", x, y, s);
+    }
+
+    private void clearScreen() {
+        System.out.print("\u001B[1,1H\u001B[2J");
     }
 }
